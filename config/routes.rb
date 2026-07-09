4219a97fecb81491523ebea155b2245d0f9abc77
Larissa Bianca <larissa_bianca@outlook.com>
2026-07-09 08:12:41 -0300

    ✨ feat: add cart routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
+  
+  get "/cart", to: "carts#show"
+  post "/cart", to: "carts#create"
+  post "/cart/add_item", to: "carts#add_item"
+  delete "/cart/:product_id", to: "carts#destroy"
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "rails/health#show"
+  
 end
