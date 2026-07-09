2f83a0907d7353584365033f0541e22ad7aae727
Larissa Bianca <larissa_bianca@outlook.com>
2026-07-09 08:18:55 -0300

    ✨ feat: create cart_item and update associations

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :products, through: cart_items
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
   # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
